867bc9e98ddc753bec715e4a836d958ee42eb13a
Vignesh Kumar Pandian <vigneshkumar.0510@gmail.com>
2019-04-07 17:15:56 +0000

    Version 5

--- app.js
+++ app.js
@@ -2,32 +2,20 @@ const express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
+app.use(express.static(__dirname + '/public'));
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+
+
 // Yelp camp Schema Setup
-let CampgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-});
-let Campground = mongoose.model("Campground", CampgroundSchema);
+const Campground = require('./models/campground'),
+    Comment = require('./models/comment'),
+    User = require('./models/user'),
+    SeedDB = require('./seeds');
 
-/*Campground.create({
-    name: "Granite Mountain",
-    image: "https://res.cloudinary.com/simplotel/image/upload/x_0,y_0,w_2592,h_1458,r_0,c_crop,q_60,fl_progressive/w_960,f_auto,c_fit/youreka/Camp-Kambre_hcemsr"
-
-}, (err, camp) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log("newly created campground");
-        console.log(camp);
-    }
-});*/
-
-
+//SeedDB();
 app.get("/", (req, res) => {
     //res.send("this is landing page");
     res.render("landing");
@@ -41,7 +29,7 @@ app.get("/campgrounds", (req, res) => {
             console.log(err);
         }
         else {
-            res.render("campgrounds", { campgrounds });
+            res.render("campgrounds/index", { campgrounds });
         }
     });
     //res.render("campgrounds", { campgrounds });
@@ -50,8 +38,9 @@ app.get("/campgrounds", (req, res) => {
 app.post("/campgrounds", (req, res) => {
     let name = req.body.name;
     let image = req.body.image;
+    let description = req.body.description;
     //Ctreate new Campground and save it
-    Campground.create({ name, image }, (err, camp) => {
+    Campground.create({ name, image, description }, (err, camp) => {
         if (err) {
             console.log(err);
         }
@@ -64,8 +53,66 @@ app.post("/campgrounds", (req, res) => {
 });
 
 app.get("/campgrounds/new", (req, res) => {
-    res.render("new.ejs");
+    res.render("campgrounds/new");
 });
+
+app.get("/campgrounds/:id", (req, res) => {
+    Campground.findById(req.params.id).populate('comments').exec((err, found) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("campgrounds/show", { campground: found });
+        }
+    });
+
+});
+
+//=================================
+//    NEW COMMENT ROUTES
+//=================================
+
+app.get('/campgrounds/:id/comments/new', (req, res) => {
+    Campground.findById(req.params.id, (err, campground) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render('comments/new', { campground });
+        }
+    });
+
+});
+app.post('/campgrounds/:id/comments', (req, res) => {
+    //lookup campground
+    Campground.findById(req.params.id, (err, campground) => {
+        if (err) {
+            console.log(err);
+            res.redirect('/campgrounds');
+        }
+        else {
+            //create new comment
+            Comment.create(req.body.comment, (err, comment) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    //link comment to object
+                    campground.comments.push(comment);
+                    campground.save();
+                    //redirect
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            });
+        }
+    })
+
+});
+/*
+app.get('/campgrounds/:id/comments/new',(req,res)=>{
+    
+});*/
+
 app.listen(process.env.PORT, process.env.IP, () => {
     console.log("Yelpcamp Server has Started");
 });
---
+++ models/campground.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const CampgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    }],
+});
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
